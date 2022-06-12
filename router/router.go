5ee8db4217569eb0e54c5ebace7f391931839cea
aeof <30540276+dialtcp@users.noreply.github.com>
2022-06-12 15:58:35 +0800

    Refactor: 简化route注册

--- router/router.go
+++ router/router.go
@@ -6,54 +6,42 @@ import (
 )
 
 func InitRouter(r *gin.Engine) {
-	apiRouter := r.Group("/douyin")
-
 	// Unauthorized APIs
+	noAuthAPIs := r.Group("/douyin")
 	{
 		// public directory is used to serve static resources
 		r.Static("/static", "./public")
 
 		// user module
-		apiRouter.GET("/feed/", controller.Feed)
-		apiRouter.POST("/user/register/", controller.Register)
-		apiRouter.POST("/user/login/", controller.Login)
+		noAuthAPIs.GET("/feed/", controller.Feed)
+		noAuthAPIs.POST("/user/register/", controller.Register)
+		noAuthAPIs.POST("/user/login/", controller.Login)
 	}
 
-	// Authorized APIs without token query parameter
-	apiRouter.POST("/publish/action/", controller.Publish)
-
-	// Authorized APIs with token query parameter
-	userRouter := r.Group("/douyin/user")
-	userRouter.Use(controller.AuthMiddleware)
+	// Form auth APIs
+	formAuthAPIs := r.Group("/douyin")
+	formAuthAPIs.Use(controller.FormAuthMiddleware)
 	{
-		userRouter.GET("/", controller.UserInfo)
+		formAuthAPIs.POST("/publish/action/", controller.Publish)
 	}
 
-	publishGroup := r.Group("/douyin/publish")
-	publishGroup.Use(controller.AuthMiddleware)
+	// Query auth APIs
+	queryAuthAPIs := r.Group("/douyin")
+	queryAuthAPIs.Use(controller.QueryAuthMiddleware)
 	{
-		publishGroup.GET("/list/", controller.PublishList)
-	}
-
-	favoriteGroup := r.Group("/douyin/favorite")
-	favoriteGroup.Use(controller.AuthMiddleware)
-	{
-		favoriteGroup.POST("/action/", controller.FavoriteAction)
-		favoriteGroup.GET("/list/", controller.FavoriteList)
-	}
-
-	commentGroup := r.Group("/douyin/comment")
-	commentGroup.Use(controller.AuthMiddleware)
-	{
-		commentGroup.POST("/action/", controller.CommentAction)
-		commentGroup.GET("/list/", controller.CommentList)
-	}
-
-	relationGroup := r.Group("/douyin/relation")
-	relationGroup.Use(controller.AuthMiddleware)
-	{
-		relationGroup.POST("/action/", controller.RelationAction)
-		relationGroup.GET("/follow/list/", controller.FollowList)
-		relationGroup.GET("/follower/list/", controller.FollowerList)
+		// user module
+		queryAuthAPIs.GET("/user/", controller.UserInfo)
+		// publish module
+		queryAuthAPIs.GET("/publish/list/", controller.PublishList)
+		// favorite module
+		queryAuthAPIs.POST("/favorite/action/", controller.FavoriteAction)
+		queryAuthAPIs.GET("/favorite/list/", controller.FavoriteList)
+		// comment module
+		queryAuthAPIs.POST("/comment/action/", controller.CommentAction)
+		queryAuthAPIs.GET("/comment/list/", controller.CommentList)
+		// relation module
+		queryAuthAPIs.POST("/relation/action/", controller.RelationAction)
+		queryAuthAPIs.GET("/relation/follow/list/", controller.FollowList)
+		queryAuthAPIs.GET("/relation/follower/list/", controller.FollowerList)
 	}
 }
